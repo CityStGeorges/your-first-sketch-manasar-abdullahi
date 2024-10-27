3a7c6b68c57b18d9c8c64b725de85c9f35962f96
manasar-abdullahi <manasar.abdullahi@city.ac.uk>
2024-10-27 13:23:24 +0000

    Add files via upload

--- HW 4/Task 2.sql
+++ HW 4/Task 2.sql
@@ -1,1 +1,27 @@
+INSERT INTO petPet VALUES 
+("Hammy", "Diane", "hamster", "M", 20101030, NULL),
+("Fluffy","Harold","Cat","F","1993-02-04",NULL),
+("Puffball","Diane","hamster","F","1999-03-30","2020-09-01"),
+("Claws", "Gwen", "cat", "M", "1994-03-17", NULL),
+("Buffy", "Harold", "dog", "F", "1989-05-13", NULL);
+
+INSERT INTO petEvent VALUES 
+(1, "Fluffy", "2020-10-15", "vet", "antibiotics", "Fluffy"),
+(2, "Hammy", "2020-10-15", "vet", "antibiotics", "Hammy"),
+(3, "Fluffy", NULL, "litter", "5 kittens born, 2 male, 3 female ", "Fluffy"),
+(4, "Claws", "1997-08-03","injury", "Broke rib", "Claws"),
+(5, "Puffball","2020-09-01","Death","Died","Puffball"),
+(6, "Buffy",NULL,"Harold request", "removed Buffy due to GDPR", "Buffy");
+
+
+
+
+
+
+
+
+
+
+
+
 
